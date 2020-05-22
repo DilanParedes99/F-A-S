c02780c1752af67c0c515d55d0188271d22d929e
DilanParedes99 <60791442+DilanParedes99@users.noreply.github.com>
2020-05-21 19:46:22 -0500

    Add files via upload
    Prueba enviar correo

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+    $destino= "dilanparedes2011@hotmail.com";
+    $nombre= $_POST["nombre"];
+    $correo= $_POST["correo"];
+    $telefono= $_POST["telefono"];
+    $mensaje= $_POST["mensaje"];
+    $contenido= "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono" . $telefono . "\nMensaje" . $mensaje;
+    mail($destino,"Contacto",$contenido);
+    header("Location:index.html");
+?>
